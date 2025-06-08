b462ca4b5459be8d174fdd89fc92a9a0ca903710
Anuar7777 <aman_anuar@mail.ru>
2025-06-08 20:47:33 +0500

    feat: add home page

---
+++ components/CollaborativeRoom.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { RoomProvider, ClientSideSuspense } from "@liveblocks/react/suspense";
+
+const CollaborativeRoom = () => {
+  return (
+    <RoomProvider id="my-room">
+      <ClientSideSuspense fallback={<div>Loading…</div>}>
+        <div className="collaborative-room">
+          <Header>
+            <div className="flex w-fit items-center justify-center gap-2">
+              <p className="document-title">Share</p>
+              <SignedOut>
+                <SignInButton />
+                <SignUpButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
+          </Header>
+
+          <Editor />
+        </div>
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+};
+
+export default CollaborativeRoom;
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,18 @@
+declare type UserType = "creator" | "editor" | "viewer";
+
 declare type HeaderProps = {
   children: React.ReactNode;
   className?: string;
 };
-    
+
+declare type RoomAccesses = Record<string, AccessType>;
+
+declare type CreateDocumentParams = {
+  userId: string;
+  email: string;
+};
+
+declare type AddDocumentBtnProps = {
+  userId: string;
+  email: string;
+};
